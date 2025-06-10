48cf555d1aff25bcc5783804d81123667b24f44a
Dariana <cdarianamedina@gmail.com>
2025-06-10 13:55:06 -0500

    Add card and buttons components (#4)
    * Add card and buttons components
    
    * Remove secondary button and structure to products
    
    * chore: some fixes
    
    * Refactor button component and update card to use new button implementation
    
    ---------
    
    Co-authored-by: Michael Liendo <michael.m.liendo.r@gmail.com>

---
+++ src/components/card.tsx
@@ -0,0 +1,46 @@
+import Button from "./button";
+
+export const Card = ({
+	title,
+	desc,
+	img,
+	price,
+	tags,
+}: {
+	title: string;
+	desc: string;
+	img: string;
+	price: number;
+	tags: string[];
+}) => {
+	return (
+		<div className="bg-white shadow-md rounded-lg max-w-sm mx-auto">
+			<img
+				src={img}
+				alt={title}
+				className="w-full object-cover rounded-t-lg h-48"
+			/>
+
+			<div className="p-6">
+				<h1 className="text-xl font-bold mb-1">{title}</h1>
+				<p>{desc}</p>
+				<p className="text-md text-sunset-purple font-bold">${price}</p>
+				<div className="flex gap-2 mt-4">
+					<div className="flex gap-2">
+						{tags.map((tag) => (
+							<p
+								key={tag}
+								className="text-sm bg-sunset-purple text-white px-2 py-1 rounded-full"
+							>
+								{tag}
+							</p>
+						))}
+					</div>
+				</div>
+				<div className="mt-8 flex gap-2 justify-end">
+					<Button>Primario</Button>
+				</div>
+			</div>
+		</div>
+	);
+};
