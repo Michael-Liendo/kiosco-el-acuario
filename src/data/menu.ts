db42eb1715b616c6fa340dc917cd04c2bbad5f2b
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-06-10 20:54:19 -0400

    feat: update menu item descriptions for consistency and clarity

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -7,7 +7,8 @@ export interface MenuItem {
 export const menu: MenuItem[] = [
 	{
 		title: "Empanadas",
-		description: "Cazón, Camarón, Mechada, Pollo, Molida, Salchichas, Queso",
+		description:
+			"Cazón, Camarón, Mechada, Pollo, Molida, Salchichas, Queso y Plátano con queso",
 		price: 2.5,
 		image: "empanadas.jpg",
 	},
@@ -25,58 +26,58 @@ export const menu: MenuItem[] = [
 		image: "cervezas.jpg",
 	},
 	{
-		title: "Camarones ajillo",
+		title: "Camarones al ajillo",
 		description:
-			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, Papa fritas",
+			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, y Papa fritas",
 		price: 23,
 		image: "camarones-ajillo.jpg",
 	},
 
 	{
 		title: "Tostón familiar",
-		description: "15 pieza de tostón, Ensalada, Salsa, Queso, Aguacate",
+		description: "15 pieza de tostón, Ensalada, Salsa, Queso, y Aguacate",
 		price: 13,
 		image: "toston-familiar.jpg",
 	},
 	{
 		title: "Combo de roncador",
 		description:
-			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, Papa frita",
+			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, y Papa frita",
 		price: 17,
 		image: "combo-roncador.jpg",
 	},
 	{
 		title: "Pargo frito",
 		description:
-			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, Papa frita",
+			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, y Papa frita",
 		price: 18,
 		image: "pargo-frito.jpg",
 	},
 	{
 		title: "Fosforera",
 		description:
-			"Camarones, Calamar, pepitona, Pulpo, Mejillón, Jaiba, Almeja, Vaquita, Arepitas",
+			"Camarones, Calamar, pepitona, Pulpo, Mejillón, Jaiba, Almeja, Vaquita, y Arepitas",
 		price: 15,
 		image: "fosforera.jpg",
 	},
 	{
 		title: "Camarones rebosado",
 		description:
-			"Tostón, Ensalada, Queso, Aguacate, Arepita, Papa frita, Salsa tártara",
+			"Tostón, Ensalada, Queso, Aguacate, Arepita, Papa frita, y Salsa tártara",
 		price: 20,
 		image: "camarones-rebosado.jpg",
 	},
 	{
 		title: "Deditos de pescado",
 		description:
-			"Tostón, Ensalada, Queso, Aguacate, Arepita, Papa frita, Salsa tártara",
+			"Tostón, Ensalada, Queso, Aguacate, Arepita, Papa frita, y Salsa tártara",
 		price: 18,
 		image: "deditos-pescado.jpg",
 	},
 	{
 		title: "Medregal",
 		description:
-			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, Papa frita",
+			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, y Papa frita",
 		price: 16,
 		image: "medregal.jpg",
 	},
@@ -84,14 +85,14 @@ export const menu: MenuItem[] = [
 	{
 		title: "Mixto de camarón y calamar",
 		description:
-			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, Papa frita, salsa tártara",
+			"Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, Papa frita, y Salsa tártara",
 		price: 23,
 		image: "mixto.jpg",
 	},
 	{
 		title: "Rueda Carite",
 		description:
-			"Ajillo, Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, Papa frita",
+			"Ajillo, Tostón, Ensalada, Queso, Salsa, Aguacate, Arepita, y Papa frita",
 		image: "rueda-carite.jpg",
 		price: 16,
 	},
